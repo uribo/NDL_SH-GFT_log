50337d7bebe62778927dadc950904300da7e0b05
Shinya Uryu <uryu.shinya@tokushima-u.ac.jp>
2024-02-22 10:48:47 +0900

    [add]初期データ

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ NDLSHとNDLGFTのデータのダウンロードとバージョン管理
 <https://id.ndl.go.jp/information/download/>
 
 - 国立国会図書館件名標目表（NDLSH）
+    - ~~RDF/XML形式データ~~
+    - TAB区切りテキスト形式データ
 - 国立国会図書館ジャンル・形式用語表（NDLGFT）
---
+++ data-raw/download.R
@@ -0,0 +1,12 @@
+
+# NDLSH -------------------------------------------------------------------
+download.file(
+  url = "https://id.ndl.go.jp/auth/data/download/ndlsh-rdf.zip",
+  destfile = "data-raw/ndlsh-rdf.zip")
+unzip("data-raw/ndlsh-rdf.zip", exdir = "data-raw/ndlsh-rdf")
+download.file(
+  url = "https://id.ndl.go.jp/auth/data/download/ndlsh-tsv.zip",
+  destfile = "data-raw/ndlsh-tsv.zip")
+
+# NDLGFT ------------------------------------------------------------------
+
