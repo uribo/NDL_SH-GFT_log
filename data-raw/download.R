e42ac722381a7e58eff5dc0cd7af1141c8a1280e
Shinya Uryu <uryu.shinya@tokushima-u.ac.jp>
2024-02-22 10:50:13 +0900

    [modify]Update
    2024-02-22 10:50

--- data-raw/download.R
+++ data-raw/download.R
@@ -7,6 +7,7 @@ unzip("data-raw/ndlsh-rdf.zip", exdir = "data-raw/ndlsh-rdf")
 download.file(
   url = "https://id.ndl.go.jp/auth/data/download/ndlsh-tsv.zip",
   destfile = "data-raw/ndlsh-tsv.zip")
+unzip("data-raw/ndlsh-tsv.zip", exdir = "data-raw/")
 
 # NDLGFT ------------------------------------------------------------------
 
