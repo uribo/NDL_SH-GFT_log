2d73271a9a8a9fd4b3f83625045bc09fdb84759d
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2024-02-22 22:10:50 +0900

    [add]zipファイルの削除

--- data-raw/download.R
+++ data-raw/download.R
@@ -4,10 +4,12 @@ download.file(
   url = "https://id.ndl.go.jp/auth/data/download/ndlsh-rdf.zip",
   destfile = "data-raw/ndlsh-rdf.zip")
 unzip("data-raw/ndlsh-rdf.zip", exdir = "data-raw/ndlsh-rdf")
+unlink("data-raw/ndlsh-rdf.zip")
 download.file(
   url = "https://id.ndl.go.jp/auth/data/download/ndlsh-tsv.zip",
   destfile = "data-raw/ndlsh-tsv.zip")
 unzip("data-raw/ndlsh-tsv.zip", exdir = "data-raw/")
+unlink("data-raw/ndlsh-tsv.zip")
 
 # NDLGFT ------------------------------------------------------------------
 
